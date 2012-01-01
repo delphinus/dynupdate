7d11cc739e412a34a3bde9ba524f54bd5d173dca
delphinus35 <delphinus@remora.cx>
2012-01-01 22:52:05 +0900

    devel 0.4

--- lib/DynUpdate.pm
+++ lib/DynUpdate.pm
@@ -7,7 +7,7 @@ use LWP::UserAgent;
 use Path::Class;
 use URI;
 
-our $VERSION = '0.4.2011123101';
+our $VERSION = '0.4.2012010101';
 
 has agent      => (is => 'ro',
     default    => "delphinus\@remora.cx - dynupdate.pl - $VERSION");
@@ -19,31 +19,32 @@ has method     => (is => 'ro', default    => 'GET');
 has protocol   => (is => 'ro', default    => 'HTTP/1.0');
 has uri        => (is => 'ro', lazy_build => 1);
 
-has username   => (is => 'ro', isa => 'Str', required   => 1);
-has password   => (is => 'ro', isa => 'Str', required   => 1);
-has hostname   => (is => 'ro', isa => 'Str', required   => 1);
-has wildcard   => (is => 'ro', isa => 'Str', default    => 'NOCHG');
-has mx         => (is => 'ro', isa => 'Str', default    => 'NOCHG');
-has backmx     => (is => 'ro', isa => 'Str', default    => 'NOCHG');
+has username   => (is => 'ro', isa => 'Str', required => 1);
+has password   => (is => 'ro', isa => 'Str', required => 1);
+has hostname   => (is => 'ro', isa => 'Str', required => 1);
+has wildcard   => (is => 'ro', isa => 'Str', default  => 'NOCHG');
+has mx         => (is => 'ro', isa => 'Str', default  => 'NOCHG');
+has backmx     => (is => 'ro', isa => 'Str', default  => 'NOCHG');
+has offline    => (is => 'ro', isa => 'Str', default  => 'NOCHG');
 has detect_uri => (is => 'ro', isa => 'Str',
     default    => 'http://checkip.dyndns.org/');
+
+has debug      => (is => 'ro', isa => 'Bool', default => 0);
 
 sub run { my $self = shift;
     return $self->update;
 }
 
 sub update { my $self = shift;
-    my $ua = $self->get_ua();
-
     my $req = HTTP::Request->new;
     $req->method($self->method);
     $req->uri($self->uri);
     $req->protocol($self->protocol);
     $req->header(Host => $self->host);
-    $req->header('User-Agent' => $ua->agent);
+    $req->header('User-Agent' => $self->ua->agent);
     $req->authorization_basic($self->username, $self->password);
 
-    my $res = $ua->request($req);
+    my $res = $self->ua->request($req);
     $res->is_success or return $self->_die($res->status_line);
 
     my $content = $res->content;
@@ -51,16 +52,16 @@ sub update { my $self = shift;
     my ($status, $ip_address) = $content =~ /(\w+)(?: (\d+\.\d+\.\d+\.\d+))?/;
 
     if ($status eq 'good') {
-        $self->log('Success',
+        $self->log(Success =>
             "ip address has been updated successfully. => $ip_address");
         return 1;
 
     } elsif ($status eq 'nochg') {
-        $self->log('Success', 'ip address does not need to be updated.');
+        $self->log(Success => 'ip address does not need to be updated.');
         return 1;
 
     } else {
-        $self->log('Failed', "ip address update failed. => '$content'");
+        $self->log(Failed => "ip address update failed. => '$content'");
         return 0;
     }
 }
@@ -76,15 +77,19 @@ sub _build_uri { my $self = shift;
         wildcard => $self->wildcard,
         mx       => $self->mx,
         backmx   => $self->backmx,
+        offline  => $self->offline,
     );
+
+    $self->debug($uri);
 
     return $uri;
 }
 
 sub get_my_ip { my $self = shift;
-    my $ua = $self->get_ua();
-    my $res = $ua->get($self->detect_uri);
+    my $res = $self->ua->get($self->detect_uri);
     $res->is_success or return $self->_die($res->status_line);
+
+    $self->debug($res->content);
 
     my ($ip_address) = $res->content =~
         m!Current IP Address: (\d+\.\d+\.\d+\.\d+)!;
@@ -93,16 +98,27 @@ sub get_my_ip { my $self = shift;
     return $ip_address;
 }
 
-sub get_ua { my $self = shift;
-    my $ua = LWP::UserAgent->new;
-    $ua->env_proxy;
-    $ua->agent($self->agent);
-
-    return $ua;
+*lwp = _lwp();
+sub _lwp {
+    my $lwp;
+    return sub { my $self = shift;
+        unless ($lwp) {
+            $lwp = LWP::UserAgent->new;
+            $ua->env_proxy;
+            $ua->agent($self->agent);
+            $self->debug('LWP instance created');
+        }
+        return $ua;
+    };
 }
 
 sub _die { my $self = shift;
-    $self->log('Error', shift);
+    $self->log(Error => shift);
+}
+
+sub debug { my ($self, $msg) = @_;
+    $self->debug or return;
+    $self->log(Debug => $msg);
 }
 
 sub log { my $self = shift;
--- lib/DynUpdate/Daemon.pm
+++ lib/DynUpdate/Daemon.pm
@@ -10,24 +10,52 @@ use File::Basename;
 use FindBin qw!$Bin!;
 use HTTP::Date qw!time2iso!;
 
-our $VERSION = '0.4.2011123101';
+our $VERSION = '0.4.2012010101';
 
 has [qw!
-    +ignore_zombies   +no_double_fork   +progname               +basedir
-    +stop_timeout     +pidfile          +dont_close_all_files   +agent
-    +scheme           +host             +path                   +method
-    +protocol         +uri
-!] => (metaclass => 'NoGetopt');
+    +ignore_zombies +no_double_fork +progname +basedir
+    +stop_timeout   +pidfile        +agent    +dont_close_all_files 
+    +scheme         +host           +path     +method
+    +protocol       +uri
+!] => (traits => ['NoGetopt']);
 
-has log_file => (is => 'ro', isa => File, coerce => File, default    => sub {
+has '+pidbase'    => (documentation => 'path to pidfile dir');
+has '+foreground' => (documentation => 'foreground execution');
+
+has '+username'   => (traits => ['Getopt'], cmd_aliases => 'n',
+    documentation => 'username registered in Dyn.com');
+has '+password'   => (traits => ['Getopt'], cmd_aliases => 'p',
+    documentation => 'password registered in Dyn.com');
+has '+hostname'   => (traits => ['Getopt'], cmd_aliases => 'h',
+    documentation => 'hostname to be updated');
+has '+wildcard'   => (traits => ['Getopt'], cmd_aliases => 'w',
+    documentation => '(currently ignored)');
+has '+mx'         => (traits => ['Getopt'], cmd_aliases => 'm',
+    documentation => '(currently ignored)');
+has '+backmx'     => (traits => ['Getopt'], cmd_aliases => 'b',
+    documentation => '(currently ignored)');
+has '+offline'    => (traits => ['Getopt'], cmd_aliases => 'o',
+    documentation => 'set to offline mode');
+has '+detect_uri' => (traits => ['Getopt'], cmd_aliases => 'u',
+    documentation => 'url for detecting ip address');
+
+has debug         => (traits => ['Getopt'], cmd_aliases => 'd',
+    documentation => 'debug mode');
+
+has log_file      => (traits => ['Getopt'], cmd_aliases => 'l',
+    documentation => 'log filename',
+    is => 'ro', isa => File, coerce => File,
+    default       => sub {
         my $name = fileparse($0, qr!\.[^.]*!);
-
         return "$Bin/logs/$name.log";
     });
 
-has my_ip    => (is => 'rw', isa => ip4, default => '0.0.0.0');
+has interval      => (traits => ['Getopt'], cmd_aliases => 'i',
+    documentation => 'interval seconds between updates',
+    is => 'ro', isa => 'Int', default => 900);
 
-has interval => (is => 'ro', isa => 'Int', default => 900);
+has my_ip         => (traits => ['NoGetopt'],
+    is => 'rw', isa => ip4, default  => '0.0.0.0');
 
 sub BUILD { my $self = shift;
     -d $self->pidbase or $self->pidbase->mkpath;
@@ -36,7 +64,7 @@ sub BUILD { my $self = shift;
 after start => sub { my $self = shift;
     $self->is_daemon or return;
 
-    $self->log('Info', 'START!');
+    $self->log(Info => 'START!');
     $self->run;
 };
 
@@ -51,26 +79,31 @@ override update => sub { my $self = shift;
     my $new = $self->get_my_ip;
 
     if ($self->my_ip eq $new) {
-        $self->log('Unchanged', 'ip address has not changed.');
+        $self->log(Unchanged => 'ip address has not changed.');
         return 1;
 
     } else {
-        $self->log('Changed', 'ip address need to be updated.');
+        $self->log(Changed => 'ip address need to be updated.');
         $self->my_ip($new);
         return super;
     }
 };
 
 override log => sub { my $self = shift;
-    my $fh = $self->get_log_fh;
-    $fh->print(sprintf "%s [%s] %s\n", time2iso(time), @_);
-    $fh->close;
+    $self->log_fh->print(sprintf "%s [%s] %s\n", time2iso(time), @_);
 };
 
-sub get_log_fh { my $self = shift;
-    -d $self->log_file->parent or $self->log_file->parent->mkpath;
-    open my $fh, '>>', $self->log_file or die;
-    $fh;
+*log_fh = _log_fh();
+sub _log_fh {
+    my $fh;
+    return sub { my $self = shift;
+        unless ($fh) {
+            -d $self->log_file->parent or $self->log_file->parent->mkpath;
+            open my $fh, '>>', $self->log_file or die;
+            $self->debug('log file handle created');
+        }
+        return $fh;
+    };
 }
 
 __PACKAGE__->meta->make_immutable;
